e8ca918dc06c86cf0c30e30ac75e408d18918ec6
Maurice Kelly <X00158592@computing.stu.it-tallaght.ie>
2018-11-04 12:00:35 +0000

    Add project files.

---
+++ Lab2MK/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2MK
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lab2MK/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab2MK.MvcApplication" Language="C#" %>
